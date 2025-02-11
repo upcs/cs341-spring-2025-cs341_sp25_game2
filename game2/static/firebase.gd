db6eab9328548d82f35694d22ef6eacba260d670
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-02-10 17:15:55 -0800

    maps 5 and 7 demo done

--- game2/static/firebase.gd
+++ game2/static/firebase.gd
@@ -1,4 +1,37 @@
 extends Node
+# Allows interaction with firebase/firestore
 
-const PROJECT_ID := "upgame-f59f3"
-const FIRESTORE_URL := "https://firestore.googleapis.com/v1/projects/upgame-f59f3/databases/(default)/documents/"
+# Called when the node enters the scene tree for the first time
+func _ready():
+	print("Firebase Manager Ready")
+
+# Function to add data to Firestore
+func add_data_to_firestore():
+	var js_code = """
+	(async () => {
+		const docRef = firestore.collection('players').doc();
+		await docRef.set({
+			name: 'Joshua',
+			score: 1000
+		});
+		console.log('Document written with ID: ', docRef.id);
+	})();
+	"""
+#	Don't worry about the error, it works in HTML5
+	JavaScript.eval(js_code, true)  # Execute the JavaScript code
+
+# Function to read data from Firestore
+func get_data_from_firestore():
+	var js_code = """
+	(async () => {
+		const docRef = firestore.collection('players').doc('your-document-id');
+		const docSnap = await docRef.get();
+		if (docSnap.exists()) {
+			console.log('Document data:', docSnap.data());
+		} else {
+			console.log('No such document!');
+		}
+	})();
+	"""
+	#	Don't worry about the error, it works in HTML5
+	JavaScript.eval(js_code, true)  # Execute the JavaScript code
