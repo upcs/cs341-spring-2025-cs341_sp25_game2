0efce545b7470d08db38bb882ea24c239eeabbef
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2025-02-11 16:59:25 -0800

    added fonts, added arrow direction, added finished part of campus map and scoring system

---
+++ game2/campus.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+var timer;
+var label;
+var score;
+var scoreLabel;
+var arrow;
+var class_on_time;
+var out_of_time;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	score = 0
+	scoreLabel = $Wally/Score
+	label = $Wally/Objective
+	timer = $ClassTimer
+	arrow = $Wally/Arrow
+	class_on_time = false
+	out_of_time = false
+	scoreLabel.text = "Score: " + str(score)
+	#timer.wait_time = 30
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scoreLabel.text = "Score: " + str(score)
+	arrow.rotation = $Wally.position.angle_to_point($DB/Marker2D.position)
+	if (class_on_time):
+		label.text = "YOU MADE IT!"
+		score += 10
+		scoreLabel.text = "Score: " + str(score)
+		class_on_time = false
+	elif (out_of_time):
+		out_of_time = false
+		label.text = "You did not make it to class on time :("
+	else:
+		label.text = "Get to class in time, follow the arrow! Time Left: " + str(int(timer.get_time_left()))
+	
+
+func _on_db_body_entered(body: Node2D) -> void:
+	if body.has_method("takehit"):
+		if not out_of_time:
+			class_on_time = true
+
+
+func _on_class_timer_timeout() -> void:
+	out_of_time = true
