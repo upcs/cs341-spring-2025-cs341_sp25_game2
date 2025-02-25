2c39caa4586d3ca534abc51be64fb15a1cb67543
nlTyler <tcrosbie44@gmail.com>
2025-02-25 15:34:27 -0800

    Pilot Run Game continued

---
+++ game2/scenes/dino.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const GRAVITY = 4200
+const JUMP_SPEED = -1800
+
+
+func _physics_process(delta: float):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		#$RunCol.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+		elif Input.is_action_pressed("ui_down"):
+			#$RunCol.disabled = true
+			const intr = 2
+	move_and_slide()
+
+#func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	#move_and_slide()
