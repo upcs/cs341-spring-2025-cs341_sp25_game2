e7f37893b096dd1c5369ea27541372b82aef7ca9
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-17 15:53:43 -0700

    stacking game functional (barebones)

---
+++ game2/scripts/book.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+var is_stacked = false
+var is_being_dragged = false
+
+func _ready():
+	pass
+
+func _on_body_entered(body: Node) -> void:
+	var library_game_node = get_parent()
+	print("_on_body_entered from book.gd triggered")
+	print(library_game_node)
+	
+	if body.is_in_group("stacked_book") or body.is_in_group("ground"):
+		if self.is_in_group("active_book") and library_game_node != null:
+			library_game_node.stacked_books += 1
+			library_game_node.game_over()
+		
+		self.remove_from_group("active_book")
+		self.freeze = true
+		self.linear_velocity = Vector2.ZERO
+		self.add_to_group("stacked_book")
--- game2/scripts/library_game.gd
+++ game2/scripts/library_game.gd
@@ -14,17 +14,18 @@ const WIN_COUNT = 10
 var game_active = true
 
 func _ready():
-	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
 	noise_meter.max_value = MAX_NOISE
 	noise_meter.value = noise_level
 	_spawn_book()
 	game_active = true
 
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		var books = get_tree().get_nodes_in_group("active_book")
 		if books.size() > 0:
-			books[0].global_position.x = event.position.x
+			var active_book = books[0]
+			active_book.global_position.x = event.position.x  # Move the current active book
 
 func _physics_process(delta):
 	pass
@@ -39,32 +40,36 @@ func _spawn_book():
 	var book = book_scene.instantiate()
 	book.global_position = spawn_point.global_position
 	book.add_to_group("active_book")
-	
 	add_child(book)
-	print("Spawned book at: ", book.global_position)
 
+	print("Spawned book at: ", book.global_position)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Library_interior.tscn")
 
-
-func _on_ground_body_entered(book: Node2D) -> void:
-	if book.is_in_group("ground"):
-		return
-	
-	print("Book landed")
-	if !game_active: return
-	var velocity = book.linear_velocity.length()
-	var noise_increase = clamp(velocity / 50.0, 0, 50)
-	noise_level = clamp(noise_level + noise_increase, 0, MAX_NOISE)
-	book.remove_from_group("active_book")
-	book.freeze = true
-	book.remove_from_group("active_book")
-	book.freeze = true
-	book.linear_velocity = Vector2.ZERO
-	book.add_to_group("stacked_book")
-	stacked_books += 1
+func game_over():
 	stack_counter.text = "Books: %d/%d" % [stacked_books, WIN_COUNT]
 	if stacked_books >= WIN_COUNT:
 		game_active = false
 		game_message.text = "You Win!"
+	else:
+		_spawn_book()
+
+#func _on_ground_body_entered(book: Node2D) -> void:
+	#if book.is_in_group("ground"):
+		#return
+	#
+	#if book.is_in_group("active_book"):
+		#print("Book is active book")
+		#if !game_active: return
+		#var velocity = book.linear_velocity.length()
+		#var noise_increase = clamp(velocity / 50.0, 0, 50)
+		#noise_level = clamp(noise_level + noise_increase, 0, MAX_NOISE)
+		#stacked_books += 1
+		#stack_counter.text = "Books: %d/%d" % [stacked_books, WIN_COUNT]
+		#if stacked_books >= WIN_COUNT:
+			#game_active = false
+			#game_message.text = "You Win!"
+		#else:
+			#_spawn_book()  # Spawn a new book, which will automatically be the new "active_book"
+		
