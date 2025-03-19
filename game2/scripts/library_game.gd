bf7607e1d01367d68137a9e3c72265e73466752f
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-19 13:26:30 -0700

    library game update
    all core functionality implemented

--- game2/scripts/library_game.gd
+++ game2/scripts/library_game.gd
@@ -1,67 +1,100 @@
 extends Node2D
 
 @onready var spawn_point = $SpawnPoint
-@onready var noise_meter = $NoiseMeter
 @onready var stack_counter = $StackCounter
 @onready var game_message = $GameMessage
 @onready var spawn_timer = $SpawnTimer
 @onready var game_score = $GameScore
+@onready var back_button = $BackButton
+@onready var restart_button = $RestartButton
+
 
 var book_scene = preload("res://scenes/Book.tscn")
 var stacked_books = 0
-var noise_level = 0.0
-const MAX_NOISE = 100.0
 const WIN_COUNT = 10
 var game_active = true
 var game_lost = false
 var multiplier = 10
 
 func _ready():
-	noise_meter.max_value = MAX_NOISE
-	noise_meter.value = noise_level
 	_spawn_book()
 	game_active = true
+	back_button.disabled = true
+	restart_button.disabled = true
 
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouse:
 		var books = get_tree().get_nodes_in_group("active_book")
+		
 		if books.size() > 0:
 			var active_book = books[0]
-			active_book.global_position.x = event.position.x  # Move the current active book
-
-func _physics_process(delta):
-	pass
+			active_book.global_position.x = event.position.x
+			
 
 func _on_spawn_timer_timeout():
 	var active_count = get_tree().get_nodes_in_group("active_book").size()
-	print("Active books: ", active_count)
+	
+	# check if a book has fallen too far
+	var active_books = get_tree().get_nodes_in_group("active_book")
+	var stacked_books = get_tree().get_nodes_in_group("stacked_book")
+	if active_books.size() > 0:
+		var active_book = active_books[0]
+		if active_book.global_position.y > 600:
+			game_lost = true
+			game_over()
+			return
+	if stacked_books.size() > 0:
+		for i in range(active_books.size()):
+			var stacked_book = stacked_books[i]
+			if stacked_book.global_position.y > 550:
+				game_lost = true
+				game_over()
+				return
+	
+	#print("Active books: ", active_count)
 	if game_active and active_count == 0:
 		_spawn_book()
 
 func _spawn_book():
 	var book = book_scene.instantiate()
-	var random_int = randi_range(-200, 200)
-	var spawn = Vector2((512 + random_int), 100)
+	call_deferred("add_child", book)
+	await get_tree().process_frame
+	await book.random_scale()
+	var random_int = randi_range(-300, 300)
+	var spawn = Vector2((512 + random_int), -100)
 	book.global_position = spawn
+	
 	book.add_to_group("active_book")
-	add_child(book)
+	#print("Spawned book at: ", book.global_position)
+	
 
-	print("Spawned book at: ", book.global_position)
-
-func _on_button_pressed() -> void:
+func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Library_interior.tscn")
-
+	
 func game_over():
 	stack_counter.text = "BOOKS: %d/%d" % [stacked_books, WIN_COUNT]
 	game_score.text = "SCORE: %d" % [multiplier * stacked_books]
 	
 	if game_lost:
 		game_active = false
-		game_message.text = "You Lost"
+		_view_buttons()
+		game_message.text = "[center]You Lost"
 	elif stacked_books >= WIN_COUNT:
 		game_active = false
-		game_message.text = "You Win! Points added to total score!"
+		_view_buttons()
+		game_message.text = "[center]You Win! Points added to total score!"
 		Global.score += multiplier * stacked_books
 	else:
 		_spawn_book()
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	
+
+func _view_buttons():
+	back_button.disabled = false
+	restart_button.disabled = false
+	back_button.show()
+	restart_button.show()
