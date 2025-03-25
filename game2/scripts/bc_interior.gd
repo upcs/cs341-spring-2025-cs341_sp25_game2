05a4999c4d6fa500f958c506e19e9833905c5120
JacquiB04 <boucharj27@up.edu>
2025-03-24 23:51:17 -0700

    Script-naming and spawn point fixes with BC

--- game2/scripts/bc_interior.gd
+++ game2/scripts/bc_interior.gd
@@ -9,3 +9,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_bc_exit_body_entered(body: Node2D) -> void:
+	print("Body entered:", body.name)  # Print the name of the body entering the area
+	if body.is_in_group("Player"):
+		print("Player entered. Changing spawn and changing scene...")
+		Global.spawn_position = Vector2(1600, 192)
+		get_tree().change_scene_to_file("res://scenes/campus.tscn")
+	else:
+		print("This body is not in the Player group")
+
+
+func _on_minigame_entrance_body_entered(body: Node2D) -> void:
+	print("Body entered:", body.name)  # Print the name of the body entering the area
+	if body.is_in_group("Player"):
+		print("Player entered. Changing spawn and changing scene...")
+		#Global.spawn_position = Vector2(2400, 940)
+		get_tree().change_scene_to_file("res://scenes/BC_maze_minigame.tscn")
+	else:
+		print("This body is not in the Player group")
--- game2/scripts/bc_maze_minigame.gd
+++ game2/scripts/bc_maze_minigame.gd
@@ -23,7 +23,7 @@ func _on_maze_exit_body_exited(body: Node2D) -> void:
 	print("Body entered:", body.name)  # Print the name of the body entering the area
 	if body.is_in_group("Player"):
 		print("Player entered. Changing spawn and changing scene...")
-		#Global.spawn_position = Vector2(2400, 940)
+		Global.spawn_position = Vector2(1600, 192)
 		get_tree().change_scene_to_file("res://scenes/campus.tscn")
 	else:
 		print("This body is not in the Player group")
