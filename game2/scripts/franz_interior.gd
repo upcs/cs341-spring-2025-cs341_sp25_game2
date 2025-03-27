baf128f94a985cd3527cc38dd9f9fe6de015a309
JacquiB04 <boucharj27@up.edu>
2025-03-27 14:01:53 -0700

    Adding back in the spawn position after leaving franz

--- game2/scripts/franz_interior.gd
+++ game2/scripts/franz_interior.gd
@@ -16,7 +16,7 @@ func _on_franz_exit_body_entered(body: Node2D) -> void:
 	print("Body entered:", body.name)  # Print the name of the body entering the area
 	if body.is_in_group("Player"):
 		print("Player entered. Changing spawn and changing scene...")
-		#Global.spawn_position = Vector2(2400, 940)
+		Global.spawn_position = Vector2(3712, 1856)
 		get_tree().change_scene_to_file("res://scenes/campus.tscn")
 	else:
 		print("This body is not in the Player group")
