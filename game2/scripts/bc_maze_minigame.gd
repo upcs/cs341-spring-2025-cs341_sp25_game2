491ada256fb73a1a19782ae502b5f0ef16036ac5
JacquiB04 <boucharj27@up.edu>
2025-04-01 13:56:07 -0700

    Update to BC maze minigame
    Added a Leave Minigame button. The game is operational, but not super exciting right now.

--- game2/scripts/bc_maze_minigame.gd
+++ game2/scripts/bc_maze_minigame.gd
@@ -3,12 +3,14 @@ extends Node2D
 @onready var game_over_message = $GameOverMessage
 @onready var game_timer = $GameTimer
 @onready var timer_label = $TimerLabel
+@onready var leave_button = $Leave
 
 var multiplier = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	leave_button.visible = false
+	var camera = get_viewport().get_camera_2d()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,19 +19,15 @@ func _process(delta: float) -> void:
 
 
 func _on_maze_exit_body_exited(body: Node2D) -> void:
-	game_over_message.text = "YOU WON! Score: "
-	#game_timer.get_time_left()
-	
-	print("Body entered:", body.name)  # Print the name of the body entering the area
-	if body.is_in_group("Player"):
-		print("Player entered. Changing spawn and changing scene...")
-		Global.spawn_position = Vector2(1600, 192)
-		get_tree().change_scene_to_file("res://scenes/campus.tscn")
-	else:
-		print("This body is not in the Player group")
+	Global.score += 10
+	game_over_message.text = "YOU DID IT! You gained 10 points!"
+	leave_button.visible = true #let the player leave
 
 
 func _on_game_timer_timeout() -> void:
-	#game over
 	game_over_message.text = "GAME OVER...better luck next time..."
-	get_tree().change_scene_to_file("res://scenes/BC_interior.tscn")
+	leave_button.visible = true #let the player leave
+
+func _on_leave_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/campus.tscn")
+	Global.spawn_position = Vector2(1600, 192)
