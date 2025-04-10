d5279e146d3ae3b35153bd18a1df1a730e9f5cf1
JacquiB04 <boucharj27@up.edu>
2025-04-10 09:47:59 -0700

    Maze minigame update, code fix for sprint 4 demo
    There are 26 lines of code commented out because there is a bug with the if, elif, and else statements. There are also some functions that need testing as indicated in the code comments. The game should work as it did before with small changes.

--- game2/scripts/bc_maze_minigame.gd
+++ game2/scripts/bc_maze_minigame.gd
@@ -1,27 +1,99 @@
 extends Node2D
 
 @onready var game_over_message = $GameOverMessage
+@onready var player_message = $PlayerMessage
 @onready var game_timer = $GameTimer
 @onready var timer_label = $TimerLabel
 @onready var leave_button = $Leave
+@onready var exit = $maze_exit/exit_here
 
-var multiplier = 10
+# For testing
+#var my_current = "first"
+
+# Get the collision boxes of each maze
+@onready var first_maze_walls = [$First_maze_walls/first_perimeter, $First_maze_walls/first_middle_shape_1,
+$First_maze_walls/first_middle_shape_2]
+@onready var scnd_maze_walls = [$Scnd_maze_walls/scnd_perimeter, $Scnd_maze_walls/scnd_middle_shape1,
+$Scnd_maze_walls/scnd_middle_shape2, $Scnd_maze_walls/scnd_middle_shape3, $Scnd_maze_walls/scnd_middle_shape4,
+$Scnd_maze_walls/scnd_middle_shape5, $Scnd_maze_walls/scnd_middle_shape6, $Scnd_maze_walls/scnd_middle_shape7]
+@onready var third_maze_walls = [$Third_maze_walls/third_perimeter, $Third_maze_walls/third_middle_shape]
+
+# Amount to be added to the score after each maze is completed
+var score_amount = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	leave_button.visible = false
-	var camera = get_viewport().get_camera_2d()
+	
+	#Make sure we start with only the first maze
+	$FirstLayer.enabled = true
+	$ScndLayer.enabled = false
+	$ThirdLayer.enabled = false	
+	
+	# Only the collision boxes for the first maze should be enabled
+	# at the start of the minigame.
+	for box in first_maze_walls :
+		box.disabled = false
+	
+	for box in scnd_maze_walls :
+		box.disabled = true
+	
+	for box in third_maze_walls :
+		box.disabled = true
+	
 
+#NEEDS TESTS
+func change_layers(current: TileMapLayer, next: TileMapLayer) -> void:
+	current.enabled = false
+	next.enabled = true
+
+#NEEDS TESTS
+func change_collisions(current: Array, next: Array) -> void:
+	for box in current : 
+		box.disabled = true
+	
+	for box in next :
+		box.disabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	timer_label.text = 	"Time: " + str(int(game_timer.time_left))
 
 
-func _on_maze_exit_body_exited(body: Node2D) -> void:
-	Global.score += 10
+# Maybe: change to on_maze_exit_body_entered
+func _on_maze_exit_body_exited(_body: Node2D) -> void:
+	# Increase player score
+	Global.score += score_amount
+	game_timer.stop()
 	game_over_message.text = "YOU DID IT! You gained 10 points!"
 	leave_button.visible = true #let the player leave
+	
+	#The following code has a bug that is still being worked out.
+	# Check if the game is over
+	#if $ThirdLayer.is_enabled() :
+		#game_over_message.text = "YOU DID IT! You completed all the mazes!"
+		#leave_button.visible = true #let the player leave
+		
+	## Else display the next maze to complete 
+	#elif $FirstLayer.is_enabled() :
+		## Change the position of the maze exit
+		#exit.position = Vector2(880,448)
+		## Change the display to the next maze
+		#change_layers($FirstLayer, $ScndLayer)
+		#my_current = "scnd"
+		#change_collisions(first_maze_walls, scnd_maze_walls)
+		## Change the player message
+		#player_message.text = "Find the invisible Exit! Hint: check the alcoves..."
+		
+	#else :
+		## change the position of the maze exit
+		#exit.position = Vector2(152,424)
+		## Change the display to the next maze
+		#change_layers($ScndLayer, $ThirdLayer)
+		#my_current = "third"
+		#change_collisions(scnd_maze_walls, third_maze_walls)
+		## change the player message
+		#player_message.text = "Find the invisible Exit! No hints this time ;)"
 
 
 func _on_game_timer_timeout() -> void:
