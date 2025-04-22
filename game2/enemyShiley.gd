5b74f0636ee8cb50293d86de9bf467aab659e317
kaitlyn-atalig <atalig25@up.edu>
2025-04-22 14:11:10 -0700

    added stars outside of map & more collision boxes

--- game2/enemyShiley.gd
+++ game2/enemyShiley.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
-@export var speed: float = 300.0
+
+@export var speed: float = 200.0
+@onready var agent: NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
 	add_to_group("Enemy")
+	agent.target_position = global_position  # Prevent warning until player is found
 
 func _physics_process(delta):
 	var player = get_tree().get_first_node_in_group("Player")
 	if not player:
 		return
-	
-	var direction_to_player = (player.global_position - global_position).normalized()
-	velocity = direction_to_player * speed
-	look_at(player.global_position)
-	rotation_degrees += 180  # to fix enemy position
+
+	# Update target position to follow the player
+	agent.target_position = player.global_position
+
+	# Get direction to next path point
+	var next_path_position = agent.get_next_path_position()
+	var direction = (next_path_position - global_position).normalized()
+	velocity = direction * speed
 	move_and_slide()
+
+	look_at(next_path_position)
+	rotation_degrees += 180  # optional fix for enemy facing
 
 func _on_area_2d_body_entered(body):
 	if "bullet" in body.name:
