52f7e748cf935c609c8b58aa2cae02b0272bbf69
kaitlyn-atalig <143756327+kaitlyn-atalig@users.noreply.github.com>
2025-04-22 12:50:07 -0700

    shiley game
    added the tiles and everything, but need to fix the shooting
    
    i accidentally messed it up

---
+++ game2/Bullet.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+@export var speed: float = 1000.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	if sprite:
+		sprite.play("shoot")
+	gravity_scale = 0
+	contact_monitor = true
+	max_contacts_reported = 4
+	body_entered.connect(_on_body_entered)
+	linear_velocity = Vector2.RIGHT.rotated(rotation) * speed
+	add_to_group("bullet")
+	await get_tree().create_timer(2.0).timeout
+	queue_free()
+
+func _on_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.queue_free()
+		queue_free()
